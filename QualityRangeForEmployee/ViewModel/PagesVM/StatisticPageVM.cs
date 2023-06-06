cd04dfbb26a84f5a2ec0f3e3f00a4a1f4e645b1f
Overlay404 <denis.alekseev.200411@gmail.com>
2023-06-06 22:34:43 +0300

    статистика

--- QualityRangeForEmployee/ViewModel/PagesVM/StatisticPageVM.cs
+++ QualityRangeForEmployee/ViewModel/PagesVM/StatisticPageVM.cs
@@ -1,18 +1,16 @@
-﻿using DataBase.Model;
-using QualityRangeForEmployee.Commands.Base;
-using QualityRangeForEmployee.View.Pages;
-using QualityRangeForEmployee.ViewModel.WindowsVM;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows;
+﻿using DataBase;
 using LiveCharts;
 using LiveCharts.Wpf;
-using System.Windows.Media;
+using QualityRangeForEmployee.Commands.Base;
+using QualityRangeForEmployee.View.Pages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using QualityRangeForEmployee.View.Windows;
+using QualityRangeForEmployee.ViewModel.WindowsVM;
+using MessageBox = QualityRangeForEmployee.View.Windows.MessageBox;
+using System.Collections.ObjectModel;
 
 namespace QualityRangeForEmployee.ViewModel.PagesVM
 {
@@ -21,11 +19,14 @@ namespace QualityRangeForEmployee.ViewModel.PagesVM
         public static StatisticPageVM Instance { get; private set; }
 
         #region Property
-        private SeriesCollection _chartSeries;
+        private SeriesCollection _chartSeries = new SeriesCollection();
         public SeriesCollection ChartSeries { get => _chartSeries; set => Set(ref _chartSeries, value); }
 
         private List<string> _itemSelected = new List<string>() { "Продавец", "Товар", "Пункт выдачи" };
         public List<string> ItemSelected { get => _itemSelected; set => Set(ref _itemSelected, value); }
+
+        private List<string> _typeChartSelectedCB = new List<string>() { "Строчная", "Столбчатая", "Линейная" };
+        public List<string> TypeChartSelectedCB { get => _typeChartSelectedCB; set => Set(ref _typeChartSelectedCB, value); }
 
         private List<string> _sortItemSelected;
         public List<string> SortItemSelected { get => _sortItemSelected; set => Set(ref _sortItemSelected, value); }
@@ -39,34 +40,248 @@ namespace QualityRangeForEmployee.ViewModel.PagesVM
         {
             var itemSelected = StatisticPage.Instance.ItemSelectedCB.SelectedIndex;
             var sortItemSelected = StatisticPage.Instance.SortItemSelectedCB.SelectedIndex;
+            var typeSelected = StatisticPage.Instance.TypeChartSelectedCB.SelectedIndex;
 
             #region DataSelected
-            if (itemSelected == 0 || sortItemSelected == 0)
+            if (itemSelected == 0 && sortItemSelected == 0)
             {
+                ChartSeries.Clear();
+
                 // продавец + заказы
+                var values = new ChartValues<int>();
+                foreach (var item in ConnectionDataBase.db.Salesman)
+                {
+                    switch (typeSelected)
+                    {
+                        case 0:
+                            ChartSeries.Add(new RowSeries
+                            {
+                                Title = item.NameCompany,
+                                Values = new ChartValues<int> { ConnectionDataBase.db.ProductListOrder.Local.Where(o => o.Product.Salesman == item).GroupBy(pr => pr.Order).Count() }
+                            });
+                            break;
+
+                        case 1:
+                            ChartSeries.Add(new ColumnSeries
+                            {
+                                Title = item.NameCompany,
+                                Values = new ChartValues<int> { ConnectionDataBase.db.ProductListOrder.Local.Where(o => o.Product.Salesman == item).GroupBy(pr => pr.Order).Count() }
+                            });
+                            break;
+
+                        case 2:
+                            values.Add(ConnectionDataBase.db.ProductListOrder.Local.Where(o => o.Product.Salesman == item).GroupBy(pr => pr.Order).Count());
+                            break;
+                    }
+                }
+                if (values.Count == 0) return;
+
+                ChartSeries.Add(new LineSeries
+                {
+                    Title = "Продавцы",
+                    Values = values
+                });
             }
-            else if (itemSelected == 0 || sortItemSelected == 1)
+            else if (itemSelected == 0 && sortItemSelected == 1)
             {
+                ChartSeries.Clear();
+
                 // продавец + товары
+                var values = new ChartValues<int>();
+                foreach (var item in ConnectionDataBase.db.Salesman)
+                {
+                    switch (typeSelected)
+                    {
+                        case 0:
+                            ChartSeries.Add(new RowSeries
+                            {
+                                Title = item.NameCompany,
+                                Values = new ChartValues<int> { ConnectionDataBase.db.ProductListOrder.Local.Where(o => o.Product.Salesman == item).Sum(plo => plo.Count) ?? 0 }
+                            });
+                            break;
+
+                        case 1:
+                            ChartSeries.Add(new ColumnSeries
+                            {
+                                Title = item.NameCompany,
+                                Values = new ChartValues<int> { ConnectionDataBase.db.ProductListOrder.Local.Where(o => o.Product.Salesman == item).Sum(plo => plo.Count) ?? 0 }
+                            });
+                            break;
+
+                        case 2:
+                            values.Add(ConnectionDataBase.db.ProductListOrder.Local.Where(o => o.Product.Salesman == item).Sum(plo => plo.Count) ?? 0);
+                            break;
+                    }
+                }
+                if (values.Count == 0) return;
+
+                ChartSeries.Add(new LineSeries
+                {
+                    Title = "Продавцы",
+                    Values = values
+                });
             }
-            else if (itemSelected == 1 || sortItemSelected == 0)
+            else if (itemSelected == 1 && sortItemSelected == 0)
             {
+                ChartSeries.Clear();
+
                 // товар + покупаемость
+                var values = new ChartValues<int>();
+                foreach (var item in ConnectionDataBase.db.Product)
+                {
+                    switch (typeSelected)
+                    {
+                        case 0:
+                            ChartSeries.Add(new RowSeries
+                            {
+                                Title = item.Name,
+                                Values = new ChartValues<int> { ConnectionDataBase.db.ProductListOrder.Local.Where(o => o.Product == item).Sum(plo => plo.Count) ?? 0 }
+                            });
+                            break;
+
+                        case 1:
+                            ChartSeries.Add(new ColumnSeries
+                            {
+                                Title = item.Name,
+                                Values = new ChartValues<int> { ConnectionDataBase.db.ProductListOrder.Local.Where(o => o.Product == item).Sum(plo => plo.Count) ?? 0 }
+                            });
+                            break;
+
+                        case 2:
+                            values.Add(ConnectionDataBase.db.ProductListOrder.Local.Where(o => o.Product == item).Sum(plo => plo.Count) ?? 0);
+                            break;
+                    }
+                }
+                if (values.Count == 0) return;
+
+                ChartSeries.Add(new LineSeries
+                {
+                    Title = "Продукты",
+                    Values = values
+                });
             }
-            else if (itemSelected == 1 || sortItemSelected == 1)
+            else if (itemSelected == 1 && sortItemSelected == 1)
             {
+                ChartSeries.Clear();
+
                 // товар + количество
+                var values = new ChartValues<int>();
+                foreach (var item in ConnectionDataBase.db.Product)
+                {
+                    switch (typeSelected)
+                    {
+                        case 0:
+                            ChartSeries.Add(new RowSeries
+                            {
+                                Title= item.Name,
+                                Values = new ChartValues<int> { item.Count ?? 0 }
+                            });
+                            break;
+
+                        case 1:
+                            ChartSeries.Add(new ColumnSeries
+                            {
+                                Title = item.Name,
+                                Values = new ChartValues<int> { item.Count ?? 0 }
+                            });
+                            break;
+
+                        case 2:
+                            values.Add(item.Count ?? 0);
+                            break;
+                    }
+                }
+                if (values.Count == 0) return;
+
+                ChartSeries.Add(new LineSeries
+                {
+                    Title = "Продукты",
+                    Values = values
+                });
             }
-            else if (itemSelected == 1 || sortItemSelected == 2)
+            else if (itemSelected == 1 && sortItemSelected == 2)
             {
+                ChartSeries.Clear();
+              
                 // товар + стоимость
+                var values = new ChartValues<decimal>();
+                foreach (var item in ConnectionDataBase.db.Product)
+                {
+                    switch (typeSelected)
+                    {
+                        case 0:
+                            ChartSeries.Add(new RowSeries
+                            {
+                                Title = item.Name,
+                                Values = new ChartValues<decimal> { item.CostWithDiscount }
+                            });
+                            break;
+
+                        case 1:
+                            ChartSeries.Add(new ColumnSeries
+                            {
+                                Title = item.Name,
+                                Values = new ChartValues<decimal> { item.CostWithDiscount }
+                            });
+                            break;
+
+                        case 2:
+                            values.Add(item.CostWithDiscount);
+                            break;
+                    }
+                }
+
+                if (values.Count == 0) return;
+
+                ChartSeries.Add(new LineSeries
+                {
+                    Title = "Продукты",
+                    Values = values
+                });
             }
             else if (itemSelected == 2)
             {
+                ChartSeries.Clear();
+
                 // точка выдачи + заказы
-            } 
+                var values = new ChartValues<int>();
+                foreach (var item in ConnectionDataBase.db.PointOfIssue)
+                {
+                    switch (typeSelected)
+                    {
+                        case 0:
+                            ChartSeries.Add(new RowSeries
+                            {
+                                Title = item.Name,
+                                Values = new ChartValues<int> { item.Order.Count() }
+                            });
+                            break;
+
+                        case 1:
+                            ChartSeries.Add(new ColumnSeries
+                            {
+                                Title = item.Name,
+                                Values = new ChartValues<int> { item.Order.Count() }
+                            });
+                            break;
+
+                        case 2:
+                            values.Add(item.Order.Count());
+                            break;
+                    }
+                }
+
+                if (values.Count == 0) return;
+
+                ChartSeries.Add(new LineSeries
+                {
+                    Title = "Точки выдачи",
+                    Values = values
+                });
+            }
             #endregion
         }
+
         #endregion
 
 
@@ -74,14 +289,14 @@ namespace QualityRangeForEmployee.ViewModel.PagesVM
         {
             Instance = this;
 
-            ChartSeries = new SeriesCollection
+            foreach (var item in ConnectionDataBase.db.Salesman)
             {
-                new RowSeries
+                ChartSeries.Add(new RowSeries
                 {
-                    Title = "My Column Series",
-                    Values = new ChartValues<decimal> { 5, 6, 10, 7 }
-                }
-            };
+                    Title = item.NameCompany,
+                    Values = new ChartValues<int> { ConnectionDataBase.db.ProductListOrder.Local.Where(o => o.Product.Salesman == item).GroupBy(pr => pr.Order).Count() }
+                });
+            }
 
             ChartUpdate = new LambdaCommand(OnChartUpdateExecute, CanChartUpdateExecute);
         }
